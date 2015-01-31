006d354e55e0727431710b7f9618e62f7c791f85
Duke Ayers <Dfa24@nau.edu>
2015-01-30 20:44:00 -0700

    Finished the project scaffold
    Every page is now available to make edits on.

--- theater_project/theater_project/urls.py
+++ theater_project/theater_project/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 # from django.contrib import admin
-from views import home
+from views import home, events, movies, showtimes, store, team
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', home),
+    url(r'^events', events),
+    url(r'^movies', movies),
+    url(r'^showtimes', showtimes),
+    url(r'^store', store),
+    url(r'^team', team),
+
     # url(r'^$', 'theater_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
--- theater_project/views.py
+++ theater_project/views.py
@@ -1,7 +1,22 @@
 from django.core import serializers
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from theater_project.models import Info
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'all_movies': Info.objects.all()})
+
+def events(request):
+    return render(request,'events.html')
+
+def movies(request):
+    return render(request,'movies.html')
+
+def showtimes(request):
+    return render(request,'showtimes.html')
+
+def store(request):
+    return render(request,'store.html')
+
+def team(request):
+    return render(request,'team.html')
