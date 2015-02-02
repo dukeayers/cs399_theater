73d54e4922eaf0539a8888b721c959fa8823ebe6
aa2468 <aa2468@nau.edu>
2015-02-02 00:20:45 -0700

    modified store dictionary

--- theater_project/views.py
+++ theater_project/views.py
@@ -1,7 +1,7 @@
 from django.core import serializers
 from django.http import HttpResponse
 from django.shortcuts import render
-from theater_project.models import Info, Movie
+from theater_project.models import Info, Movie , Store
 # Create your views here.
 def home(request):
     return render(request, 'index.html', {'all_movies': Info.objects.all()})
@@ -16,7 +16,7 @@ def showtimes(request):
     return render(request,'showtimes.html', {'all_movies': Info.objects.all(), 'movie_info': Movie.objects.filter(title="Duke's Heroes"), 'movie_info2': Movie.objects.filter(title="Movie3"), 'movie_info3': Movie.objects.filter(title="Movie3"), 'movie_info4': Movie.objects.filter(title="Movie4"), 'movie_info5': Movie.objects.filter(title="Movie5")})
 
 def store(request):
-    return render(request,'store.html', {'all_movies': Info.objects.all()})
+    return render(request,'store.html', {'store_item': Store.objects.all()})
 
 def team(request):
     return render(request,'team.html', {'all_movies': Info.objects.all()})
